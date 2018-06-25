6b86af2bf2db3befe7839e927e4339a6bb871f47
Cristiano Dalbem <ninja.dalbem@gmail.com>
2018-06-24 23:11:26 -0300

    Add Geolocation cordova plugin

--- Polyfills/cordova/platforms/android/platform_www/cordova_plugins.js
+++ Polyfills/cordova/platforms/android/platform_www/cordova_plugins.js
@@ -21,6 +21,18 @@ module.exports = [
         "clobbers": [
             "hostedwebapp"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -28,7 +40,9 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-network-information": "2.0.1",
-    "cordova-plugin-hostedwebapp": "0.3.1"
+    "cordova-plugin-hostedwebapp": "0.3.1",
+    "cordova-plugin-compat": "1.2.0",
+    "cordova-plugin-geolocation": "2.4.3"
 };
 // BOTTOM OF METADATA
 });
